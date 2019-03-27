f3da419a58fdc714cee78fc32389afcfefd1335e
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-03-27 23:42:01 +0900

    fix to string

--- src/ScrollContainer.js
+++ src/ScrollContainer.js
@@ -3,5 +3,5 @@ import { ScrollView } from 'react-native'
 
 export const ScrollContainer = ({ children, contentContainerStyle, style, ...rest }) => {
   // in ScrollView, alingment for children is defined by contentStyle, not style.
-  return <ScrollView contentContainerStyle={{ ...contentContainerStyle, }} style={{ backgroundColor: white, flex: 1, ...style }} {...rest}>{children}</ScrollView>
+  return <ScrollView contentContainerStyle={{ ...contentContainerStyle, }} style={{ backgroundColor: "white", flex: 1, ...style }} {...rest}>{children}</ScrollView>
 }
