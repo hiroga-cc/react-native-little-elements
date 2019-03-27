83d0a40cbfa627c22fd55b9a888fecabad7e9237
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-03-27 17:16:29 +0900

    chnage how to export

--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export const Hello = (props) => {
+const Hello = (props) => {
   const name = props.name != undefined ? props.name : "hiroga"
 
   return (
     <View><Text>{`Hello, ${name}!`}</Text></View>
   )
 }
+
+export { Hello }
