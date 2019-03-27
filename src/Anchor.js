e1a301730d06a8d4d8fdddd7e8864109212f00b7
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-03-27 23:29:40 +0900

    fix props reference

--- src/Anchor.js
+++ src/Anchor.js
@@ -4,15 +4,15 @@ import { Linking, TouchableOpacity } from 'react-native'
 export const Anchor = (props) => {
 
   _handlePress = () => {
-    if (this.props.href !== undefined && this.props.href.length != 0) {
-      Linking.openURL(this.props.href)
+    if (props.href !== undefined && props.href.length != 0) {
+      Linking.openURL(props.href)
     }
-    this.props.onPress && this.props.onPress()
+    props.onPress && props.onPress()
   }
 
   return (
-    <TouchableOpacity {...this.props} onPress={this._handlePress}>
-      {this.props.children}
+    <TouchableOpacity {...props} onPress={this._handlePress}>
+      {props.children}
     </TouchableOpacity>
   )
 }
